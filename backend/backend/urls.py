c90437426d62667e6cadfb1224f1622fe46df522
BhavyaProjects07 <laptopuse01824x@gmail.com>
2025-07-25 02:31:50 +0530

    Final push before deployment

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,9 +12,13 @@ class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
 
 
+def home(request):
+    return HttpResponse("Welcome to Dark AI Backend!")
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+     path('', home),
     # ✅ Your main API endpoints
     path('api/', include('api.urls')),
 
